3d4a5fa1f28b27b5bf9618bcbfedc1329d05a40f
deshithkavindu <deshithakavindu229@gmail.com>
2025-10-15 14:40:49 +0530

    updates

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO alien (id, name, tech) VALUES (101, 'kiran', 'Blockchain');
+INSERT INTO alien (id, name, tech) VALUES (102, 'kran', 'Blchain');
+INSERT INTO alien (id, name, tech) VALUES (103, 'an', 'kchain');
